91640497eeac149a5fa1a87df73202eea9a2123d
Sandip Pandey <np.sandip.pandey@gmail.com>
2019-02-13 10:20:51 +0100

    PyInstaller build for key generation executable for testing

--- generate_key.py
+++ generate_key.py
@@ -2,12 +2,21 @@ from __future__ import absolute_import
 from __future__ import print_function
 
 from binascii import hexlify
-import sys
 
 from ipv8.keyvault.crypto import ECCrypto
 
-# This script generates a curve25519 key and prints it in hex format
-if '--public' in sys.argv:
-    print(hexlify(ECCrypto().generate_key(u"curve25519").pub().key_to_bin()))
-else:
-    print(hexlify(ECCrypto().generate_key(u"curve25519").key_to_bin()))
+# This script generates a list of different curve keys and prints it in hex format
+from ipv8.util import cast_to_bin
+
+ec = ECCrypto()
+data = cast_to_bin("".join([chr(i) for i in range(256)]))
+
+key_types = [u"very-low", u"low", u"medium", u"high", u"curve25519"]
+for key_type in key_types:
+    print("\nTesting key type  :", key_type)
+    key = ec.generate_key(key_type)
+    print("Generated Key     :", hexlify(key.pub().key_to_bin()))
+    signature = key.signature(data)
+    print("Signature         :", hexlify(signature))
+    print("Signature is valid:", ec.is_valid_signature(key.pub(), data, signature) != False)
+
